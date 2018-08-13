0d4c7d64c85e33b663792e41700882cf1c3623c8
Andrew Damberger <adamberger@tesla.com>
2018-08-13 17:14:22 -0400

    Add project files.

---
+++ Fapio_Red_Stamp_Removal/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fapio_Red_Stamp_Removal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
